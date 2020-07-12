995c5a66dfa84b6249df6b8321c8fac8ad75d202
Richard James <richard@richardpjames.com>
2020-07-12 15:23:25 +0100

    Reduce data available on users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,17 +10,35 @@ module.exports = {
       res.status(404);
       return res.send();
     }
+    // If we have users then don't expose anything untoward, just the id,  nicknames and pictures
+    const returnedUsers = [];
+    await Promise.all(
+      users.map((user) =>
+        returnedUsers.push({
+          id: user.user_id,
+          nickName: user.nickname,
+          picture: user.picture,
+        }),
+      ),
+    );
     res.status(200);
-    return res.send(users);
+    return res.send(returnedUsers);
   },
   // For getting a single user
   getById: async (req, res) => {
-    const user = usersService.getById(req.params.userId, req.managementToken);
+    const user = await usersService.getById(
+      req.params.userId,
+      req.managementToken,
+    );
     if (!user) {
       res.status(404);
       return res.send();
     }
     res.status(200);
-    return res.send(user);
+    return res.send({
+      id: user.user_id,
+      nickName: user.nickname,
+      picture: user.picture,
+    });
   },
 };
