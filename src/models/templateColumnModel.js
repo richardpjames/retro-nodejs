253e4b0045e1df7d5990aa7bd39d9192d4b1f204
Richard James <richard@Richards-iMac.local>
2020-08-11 15:10:25 +0100

    Sort columns by rank

--- src/models/templateColumnModel.js
+++ src/models/templateColumnModel.js
@@ -4,7 +4,7 @@ const joi = require('joi');
 // Export the joi model
 module.exports = joi.object().keys({
   title: joi.string().required(),
-  order: joi.number().required(),
+  rank: joi.string().required(),
   created: joi.date().required(),
   templateId: joi.object().required(),
 });
